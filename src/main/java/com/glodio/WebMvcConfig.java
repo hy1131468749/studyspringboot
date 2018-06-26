c1a36ae84d143629ba05fa16dd2212c4ab974c87
hanyu <1131468749@qq.com>
2018-06-26 14:02:25 +0800

    注释添加

--- src/main/java/com/glodio/WebMvcConfig.java
+++ src/main/java/com/glodio/WebMvcConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 配置spring mvc的一些 配置 ，这里只配置了静态资源的访问路径
+ * @author Administrator
+ *
+ */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer   {
 	@Override
